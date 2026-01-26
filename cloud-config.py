d815ddd1206d1d937be2963d47a7dbae3d8e6855
Engineer-Ibk <baloweezy23@gmail.com>
2026-01-26 10:21:44 -0500

    Added cloud connection script

---
+++ cloud-config.py
@@ -0,0 +1,7 @@
+# AWS Configuration
+# WARNING: DO NOT DO THIS IN REAL LIFE
+AWS_ACCESS_KEY_ID = "AKIA5S6T7U8V9W0X1Y2Z"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+def connect_to_aws():
+    print("Connecting to Amazon with credentials...")
