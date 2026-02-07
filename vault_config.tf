25485209be27cc4e95766a23d3e3a031bfcc43b8
Engineer-Ibk <baloweezy23@gmail.com>
2026-02-06 21:50:21 -0500

    Project 10: Full Pipeline Reset - Zero Trust Verified

--- vault_config.tf
+++ vault_config.tf
@@ -1,14 +1,13 @@
-resource "aws_secretsmanager_secret" "db_key" {
-  name        = "Production_Database_API_Key"
-  description = "Managed by Security Engineering - Auto-rotating key"
+resource "aws_secretsmanager_secret" "production_vault" {
+  name        = "Enterprise_API_Key"
+  description = "Managed Secret with 24-hour Rotation Policy"
 }
 
-# GOVERNANCE: Setting the rotation policy to 1 day
-resource "aws_secretsmanager_secret_rotation" "daily_rotation" {
-  secret_id           = aws_secretsmanager_secret.db_key.id
+resource "aws_secretsmanager_secret_rotation" "rotation_logic" {
+  secret_id           = aws_secretsmanager_secret.production_vault.id
   rotation_lambda_arn = "arn:aws:lambda:us-east-1:123456789:function:rotation_logic"
 
   rotation_rules {
-    automatically_after_days = 1
+    automatically_after_days = 1 # The "Self-Destruct" Rotation Policy
   }
 }
